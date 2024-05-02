c9b19f756ce734791d5a341368552ce48f1706e1
JuanPescoran <u20221c936@upc.edu.pe>
2024-05-02 16:50:34 -0500

    fix: fixed problem that doesn't show data in the deployment

---
+++ src/app/gympal/model/nutritionist.entity.spec.ts
@@ -0,0 +1,7 @@
+import { Nutritionist } from './nutritionist.entity';
+
+describe('Nutritionist', () => {
+  it('should create an instance', () => {
+    expect(new Nutritionist()).toBeTruthy();
+  });
+});
