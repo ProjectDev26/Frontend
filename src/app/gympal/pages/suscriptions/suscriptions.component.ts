8bdf0f4e3199b6d4b7a09aeb5d210cdc8cc7c915
angel <angelcca2001@gmail.com>
2024-05-02 03:22:31 -0500

    feat(list-suscriptions): added list suscriptions component

---
+++ src/app/gympal/pages/suscriptions/suscriptions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-suscriptions',
+  templateUrl: './suscriptions.component.html',
+  styleUrl: './suscriptions.component.css'
+})
+export class SuscriptionsComponent {
+
+}
