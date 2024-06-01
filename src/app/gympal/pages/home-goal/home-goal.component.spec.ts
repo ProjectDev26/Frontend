bf3dcf60032a375c9fa4711b3aae4e26e8c00a6c
angel <angelcca2001@gmail.com>
2024-06-01 04:19:00 -0500

    chore(home): update home page layout and styles

---
+++ src/app/gympal/pages/home-goal/home-goal.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HomeGoalComponent } from './home-goal.component';
+
+describe('HomeGoalComponent', () => {
+  let component: HomeGoalComponent;
+  let fixture: ComponentFixture<HomeGoalComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [HomeGoalComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(HomeGoalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/gympal/pages/home-goal/home-goal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-goal',
+  templateUrl: './home-goal.component.html',
+  styleUrl: './home-goal.component.css'
+})
+export class HomeGoalComponent {
+
+}
