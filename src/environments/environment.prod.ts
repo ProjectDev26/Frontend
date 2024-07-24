8b3bb4857286fe2fefc4899c0663a2d93b50ef21
angel <angelcca2001@gmail.com>
2024-07-24 03:00:26 -0500

    feat: updated production

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  serverBasePath: 'https://app-flexpal-web-852097a53c96.herokuapp.com/api/v1',
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
-  export const environment = {
-    serverBasePath: undefined
-  };
+export const environment = {
+  production: false,
+  serverBasePath: 'https://app-flexpal-web-852097a53c96.herokuapp.com/api/v1',
+};
